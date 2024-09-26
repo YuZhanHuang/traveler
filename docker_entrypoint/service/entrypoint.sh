e57a8883b0645ebadeba44ef6c6f967881e193e9
YuZhanHuang <bigjoe1992111@gmail.com>
2024-09-26 19:29:52 +0800

    remove redunder code

--- docker_entrypoint/service/entrypoint.sh
+++ docker_entrypoint/service/entrypoint.sh
@@ -19,12 +19,7 @@ try:
         host=os.environ['DB_HOST'],
         port=5432
     )
-    print("os.environ['DB_NAME']", os.environ['DB_NAME'])
-    print("os.environ['DB_USER']", os.environ['DB_USER'])
-    print("os.environ['DB_PASS']", os.environ['DB_PASS'])
-    print("os.environ['DB_HOST']", os.environ['DB_HOST'])
 except Exception as e:
-    print('----- Error -----', e)
     sys.exit(-1)
 sys.exit(0)
 END
